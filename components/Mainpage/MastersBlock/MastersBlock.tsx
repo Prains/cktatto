20bdca8338a97eb7b36edc3937a3f89abef87026
prains <sleskov2013@yandex.ru>
2023-08-03 21:03:01 +0300

    masters block finally finished

--- components/Mainpage/MastersBlock/MastersBlock.tsx
+++ components/Mainpage/MastersBlock/MastersBlock.tsx
@@ -1,7 +1,8 @@
+"use client";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/pagination";
-import { Pagination } from "swiper/modules";
+import { Navigation } from "swiper/modules";
+import "swiper/css/navigation";
 import masterImage from "./2023-04-06 2.30 2.png";
 import MasterItem from "./MasterItem/MasterItem";
 import ContentZone from "@/components/ui/ContentZone/ContentZone";
@@ -15,14 +16,36 @@ const MastersBlock = () => {
       role: "Топ-мастер, преподаватель",
       price: 15000,
     },
+    {
+      image: masterImage.src,
+      name: "Максим Витолиньш",
+      experience: 15,
+      role: "Топ-мастер, преподаватель",
+      price: 15000,
+    },
+    {
+      image: masterImage.src,
+      name: "Максим Витолиньш",
+      experience: 15,
+      role: "Топ-мастер, преподаватель",
+      price: 15000,
+    },
   ];
   return (
-    <section className="flex flex-col">
-      <ContentZone className="flex flex-col">
-        <h2 className="text-semibold uppercase self-center еуч">
+    <section>
+      <ContentZone>
+        <h2 className="text-semibold mb-11 self-center text-center uppercase">
           наши мастера
         </h2>
-        <MasterItem {...masters[0]} />
+        <Swiper modules={[Navigation]} navigation>
+          {masters.map((master, index) => {
+            return (
+              <SwiperSlide key={index}>
+                <MasterItem {...master} />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </ContentZone>
     </section>
   );
