bc69c7eaeda9869862432beff0cccbe469c3d967
Pazific <advolatum@yandex.ru>
2023-08-20 16:08:58 +0300

    feat: make footer desktop

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,10 +1,12 @@
 import ContentZone from "../ui/ContentZone/ContentZone";
+import FooterDesktop from "./FooterDesktop/FooterDesktop";
 import FooterMobile from "./FooterMobile/FooterMobile";
 
 const Footer = () => {
   return ( 
     <footer>
       <FooterMobile />
+      <FooterDesktop />
     </footer>
    );
 }
