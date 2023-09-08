c5164a8610ca2b05497488ef3a60a5940b723269
prains <sleskov2013@yandex.ru>
2023-09-08 14:09:04 +0300

    add ui tools

--- components/ui/Overlay.tsx
+++ components/ui/Overlay.tsx
@@ -1,22 +1,24 @@
 "use client";
 import { useEffect } from "react";
 
-interface Overlay {
+interface OverlayProps {
   children: React.ReactNode;
   className?: string;
+  overlayColor: string;
 }
 
-const Overlay = ({ className, children }: Overlay) => {
+const Overlay = ({ children, className, overlayColor }: OverlayProps) => {
   useEffect(() => {
     document.body.style.overflow = "hidden";
     return () => {
       document.body.style.overflow = "unset";
     };
   }, []);
+
   return (
     <div
       className={
-        "z-50 absolute top-0 left-0 right-0 flex items-center justify-center w-full h-full bg-neutral-900/50" +
+        `${overlayColor} absolute z-50  top-0 left-0 w-full h-full flex items-center justify-center` +
         ` ${className}`
       }
     >
